c86153ce06fcf2ee6b0a07a60b99852d01c9352e
Ben Hall <benji2357@gmail.com>
2017-06-28 08:32:09 +0100

    difference colors and some background color

--- src/stuff/core.clj
+++ src/stuff/core.clj
@@ -9,7 +9,23 @@
     (fn []
       (def cy (Math/pow (rand) 2))
       (def rx (+ (* cy 0.1) 0.01))
-      {:cx (rand) :cy cy :fill "#111" :rx rx :ry (/ rx 2)})))
+      {:cx (rand) :cy cy :fill "#222" :rx rx :ry (/ rx 2)})))
+
+(def medium-ellipses
+  (repeatedly
+    (Math/round (/ circles-count 3.))
+    (fn []
+      (def cy (Math/pow (rand) 2))
+      (def rx (+ (* cy 0.1) 0.01))
+      {:cx (rand) :cy cy :fill "#555" :rx rx :ry (/ rx 2)})))
+
+(def light-ellipses
+  (repeatedly
+    (Math/round (/ circles-count 3.))
+    (fn []
+      (def cy (Math/pow (rand) 2))
+      (def rx (+ (* cy 0.1) 0.01))
+      {:cx (rand) :cy cy :fill "#aaa" :rx rx :ry (/ rx 2)})))
 
 (defn Ellipse [{cx :cx cy :cy fill :fill rx :rx ry :ry}]
   (str "<ellipse cx=\"" cx "\" cy=\"" cy "\" fill=\"" fill "\" rx=\"" rx "\" ry=\"" ry "\"/>"))
@@ -18,9 +34,10 @@
   (str
     "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
     "<svg height=\"1px\" viewbox=\"0 0 1 1\" version=\"1.1\" width=\"1px\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">"
+      "<rect fill=\"#000\" height=\"100%\" width=\"100%\"></rect>"
       children
     "</svg>"))
 
 (defn -main
   [& args]
-  (spit "texture.svg" (Svg {:children (apply str (map Ellipse dark-ellipses))})))
+  (spit "texture.svg" (Svg {:children (apply str (mapcat Ellipse (concat dark-ellipses medium-ellipses light-ellipses)))})))
